c24db3606bd7f74a7a689a84646e058d3e7fe6eb
ambi <forworkingliu@163.com>
2025-08-12 22:08:58 +0800

    模拟string类的行为

---
+++ CPP_Base/day05/myString.cpp
@@ -0,0 +1,99 @@
+#include <string.h>
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class String {
+public:
+    String()
+        :_pstr(nullptr) {
+    }
+
+    String(const char *pstr)
+        :_pstr(new char[strlen(pstr) + 1]()) {
+        strcpy(_pstr, pstr);
+    }
+
+    String(const String &rhs)
+        :_pstr(new char[strlen(rhs._pstr) + 1]()) {
+        strcpy(_pstr, rhs._pstr);
+    }
+
+    String &operator=(const String &rhs) {
+        if (&rhs != this) {
+            delete _pstr;
+            _pstr = new char[strlen(rhs._pstr) + 1]();
+            strcpy(_pstr, rhs._pstr);
+        }
+
+        return *this;
+    }
+
+    String &append(const String &rhs) {
+        char *temp = new char[strlen(_pstr) + strlen(rhs._pstr) + 1]();
+        strncpy(temp, _pstr, strlen(_pstr));
+        strcat(temp, rhs._pstr);
+        delete _pstr;
+        _pstr = temp;
+        return *this;
+    }
+
+    String &append(const char *pstr) {
+        char *temp = new char[strlen(_pstr) + strlen(pstr) + 1]();
+        strncpy(temp, _pstr, strlen(_pstr));
+        strcat(temp, pstr);
+        delete _pstr;
+        _pstr = temp;
+        return *this;
+    }
+    ~String() {
+        if (_pstr) {
+            delete _pstr;
+            _pstr = nullptr;
+        }
+    }
+    void print() {
+        if (_pstr) {
+            cout << _pstr << endl;
+        }
+    }
+    size_t length() const {
+        return strlen(_pstr);
+    }
+    const char *c_str() const {
+        return _pstr;
+    }
+
+private:
+    char *_pstr;
+};
+
+int main() {
+    String str1;
+    str1.print();
+
+
+    String str2 = "Hello,world";
+    String str3("wangdao");
+
+    str2.print();//"Hello,world";
+    str3.print();//"wangdao"
+
+    str2.append(str3);
+    str2.print();//"Hello,worldwangdao";
+
+    str2.append("hihihi");
+    str2.print();
+
+    String str4 = str3;
+    str4.print();//"wangdao"
+
+    str4 = str2;
+    str4.print();
+
+    cout << "str4 length: " << str4.length() << endl;
+
+    cout << "str4 c_str: " << str4.c_str() << endl;
+
+    return 0;
+}
