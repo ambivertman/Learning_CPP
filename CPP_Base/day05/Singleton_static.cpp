b5ac5c50b5ff0dc644d26fdf3c81705b3a845f22
Ambi <forworkingliu@163.com>
2025-08-18 22:28:14 +0800

    完成静态区的单例实现

--- CPP_Base/day05/Singleton_static.cpp
+++ CPP_Base/day05/Singleton_static.cpp
@@ -3,11 +3,34 @@ using std::cout;
 using std::endl;
 
 class Singleton{
+public:
+    static Singleton & getInstance(){
+        static Singleton s = Singleton();
+        
+        return s;
+    }
 
+    ~Singleton(){
+        cout << "~Singleton()" << endl;
+    }
+
+    void print(){
+        cout << "print()" << endl;
+    }
+
+private:
+    Singleton(){
+        cout << "Singleton()" << endl;
+    }
+    Singleton(Singleton const & rhs);
 };
 
-int main(){
+void test(){
+    Singleton::getInstance().print();
+}
 
+int main(){
+    test();
 
     return 0;
 }
